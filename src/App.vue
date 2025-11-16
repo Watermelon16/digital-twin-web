e401ac45f8c55c35fdbef2a712e77b1cecded6c0
Lee <phuonglh43@gmail.com>
2025-11-16 14:29:08 +0700

    Fix App.vue and style import

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <script setup>
-import { onMounted } from 'vue'
-import * as Cesium from 'cesium'
-import 'cesium/Build/Cesium/Widgets/widgets.css'
-
-onMounted(() => {
-  const viewer = new Cesium.Viewer('cesiumContainer', {
-    timeline: false,
-    animation: false,
-  })
-
-  // để sau này debug từ console
-  window.cesiumViewer = viewer
-})
+import MapViewer from './components/MapViewer.vue'
 </script>
 
 <template>
-  <div id="cesiumContainer"></div>
+  <MapViewer />
 </template>
 
 <style>
-html,
-body,
-#app {
+html, body, #app {
   margin: 0;
   padding: 0;
   width: 100%;
   height: 100%;
-}
-
-#cesiumContainer {
-  width: 100vw;
-  height: 100vh;
-  display: block;
 }
 </style>
---
+++ src/components/MapViewer.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+let viewer
+
+onMounted(() => {
+  viewer = new Cesium.Viewer('mapContainer', {
+    animation: false,
+    timeline: false,
+    baseLayerPicker: true,
+  })
+
+  window.cesiumViewer = viewer
+})
+</script>
+
+<template>
+  <div id="mapContainer"></div>
+</template>
+
+<style scoped>
+#mapContainer {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+</style>
