58beca20ae86e76b5d6f03a725d9a2eaea41aea8
Lee <phuonglh43@gmail.com>
2025-11-16 13:25:45 +0700

    Add Cesium viewer and vite-plugin-cesium

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    timeline: false,
+    animation: false,
+  })
+
+  // để sau này debug từ console
+  window.cesiumViewer = viewer
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="cesiumContainer"></div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+  display: block;
 }
 </style>
